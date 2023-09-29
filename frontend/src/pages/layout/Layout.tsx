68f99864ce559914c432aaf80edec6cc5fbcfa02
Scott Semyan <scottse@microsoft.com>
2023-09-28 21:28:59 -0700

    Remove history button

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -2,19 +2,8 @@ import { Outlet, Link } from "react-router-dom";
 import styles from "./Layout.module.css";
 import Azure from "../../assets/Azure.svg";
 import { Stack } from "@fluentui/react";
-import { useContext, useEffect, useState } from "react";
-import { HistoryButton } from "../../components/common/Button";
-import { AppStateContext } from "../../state/AppProvider";
-import { CosmosDBStatus } from "../../api";
 
 const Layout = () => {
-    const appStateContext = useContext(AppStateContext)
-
-    const handleHistoryClick = () => {
-        appStateContext?.dispatch({ type: 'TOGGLE_CHAT_HISTORY' })
-    };
-
-    useEffect(() => {}, [appStateContext?.state.isCosmosDBAvailable.status]);
 
     return (
         <div className={styles.layout}>
@@ -32,12 +21,6 @@ const Layout = () => {
                             <h1 className={styles.headerTitle}>Azure AI Dating Assistant</h1>
                         </Link>
                     </Stack>
-                    <Stack horizontal tokens={{ childrenGap: 4 }}>
-                            {(appStateContext?.state.isCosmosDBAvailable?.status !== CosmosDBStatus.NotConfigured) && 
-                                <HistoryButton onClick={handleHistoryClick} text={appStateContext?.state?.isChatHistoryOpen ? "Hide chat history" : "Show chat history"}/>    
-                            }                            
-                    </Stack>
-
                 </Stack>
             </header>
             <Outlet />
